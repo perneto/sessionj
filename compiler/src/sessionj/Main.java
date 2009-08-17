28d2268b0b9368a1c9b82289ce0eeb0b3fc71678
Olivier Pernet <o.pernet@gmail.com>
2009-08-17 22:03:42 +0100

    Better handling of exceptions in Main, copied from Polyglot

--- compiler/src/sessionj/Main.java
+++ compiler/src/sessionj/Main.java
@@ -1,5 +1,7 @@
 package sessionj;
 
+import java.io.PrintStream;
+
 /**
  * Main is the main program of the compiler extension.
  * It simply invokes Polyglot's main, passing in the extension's
@@ -7,16 +9,22 @@ package sessionj;
  */
 public class Main
 {
-  public static void main(String[] args) {
-      polyglot.main.Main polyglotMain = new polyglot.main.Main();
+    public static void main(String[] args) {
+        int exitCode = start(args, System.out, System.err);
+        System.exit(exitCode);
+    }
 
-      try {
-          polyglotMain.start(args, new ExtensionInfo());
-      }
-      catch (polyglot.main.Main.TerminationException te) {
-          if (te.getMessage() != null)
-              (te.exitCode==0 ? System.out : System.err).println(te.getMessage());
-          System.exit(te.exitCode);
-      }
-  }
+    public static int start(String[] args, PrintStream out, PrintStream err) {
+        polyglot.main.Main polyglotMain = new polyglot.main.Main();
+
+        try {
+            polyglotMain.start(args, new ExtensionInfo());
+        }
+        catch (polyglot.main.Main.TerminationException te) {
+            if (te.getMessage() != null)
+                (te.exitCode==0 ? out : err).println(te.getMessage());
+            return te.exitCode;
+        }
+        return 0;
+    }
 }
