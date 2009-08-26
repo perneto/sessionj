c314418e5f6d412e310535876ae549d2fbdb7fa2
Olivier Pernet <o.pernet@gmail.com>
2009-08-26 16:29:31 +0100

    fixes issue 4, much adhockery

--- compiler/src/sessionj/ast/sessvars/SJLocalSocket_c.java
+++ compiler/src/sessionj/ast/sessvars/SJLocalSocket_c.java
@@ -10,9 +10,31 @@ public class SJLocalSocket_c extends Local_c implements SJLocalSocket
 	{
 		super(pos, id);
 	}
-	
+
 	public String sjname()
 	{
 		return name();
 	}
+
+    /**
+     * equals is needed here because of
+     * {@link sessionj.visit.noalias.SJNoAliasExprBuilder#removeNonFinalSocketArguments(polyglot.ast.Call, java.util.List)} 
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) return true;
+        if (!(o instanceof SJLocalSocket_c)) return false;
+        SJLocalSocket_c that = (SJLocalSocket_c) o;
+
+        return (del == that.del || del != null && del.equals(that.del)) &&
+                (exceptions == that.exceptions || exceptions != null && exceptions.equals(that.exceptions)) &&
+                // Not using the ext field for comparison, as it is either null or not in the above mentioned method
+                (li == that.li || li != null && li.equals(that.li)) &&
+                (name == that.name || name != null && name.equals(that.name)) &&
+                (position == that.position || position != null && position.equals(that.position)) &&
+                (type == that.type || type != null && type.equals(that.type)) &&
+                reachable == that.reachable &&
+                error == that.error;
+    }
+    
 }
