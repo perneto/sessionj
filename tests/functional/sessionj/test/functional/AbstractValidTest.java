aba40b3886efc8f58e93bb5c530aea7b21869194
Olivier Pernet <o.pernet@gmail.com>
2009-08-27 15:49:49 +0100

    changes to test framework. regression test added for issue 4

--- tests/functional/sessionj/test/functional/AbstractValidTest.java
+++ tests/functional/sessionj/test/functional/AbstractValidTest.java
@@ -1,8 +1,44 @@
 package sessionj.test.functional;
 
+import sessionj.runtime.net.SJSessionParameters;
+import sessionj.runtime.transport.TransportUtils;
+
+import java.util.concurrent.*;
+
+import org.testng.annotations.Test;
+
 /**
  *
  */
 public abstract class AbstractValidTest {
-    public abstract void run();
+    private static final int CLIENT_SLEEP = 500;
+    private static final int TEST_PORT = 1234;
+
+    protected abstract void server(int port, SJSessionParameters params) throws Exception;
+    protected abstract void client(int port, SJSessionParameters params) throws Exception;
+    
+    @Test
+    public void run() throws Exception {
+        // "f" : shared-memory unbounded FIFO
+        final SJSessionParameters params = TransportUtils.createSJSessionParameters("f", "f");
+
+        ExecutorService es = Executors.newFixedThreadPool(2);
+        Future<?> b = es.submit(new Callable<Object>() {
+            public Object call() throws Exception {
+                server(TEST_PORT, params);
+                return null;
+            }
+        });
+        Future<?> a = es.submit(new Callable<Object>() {
+            public Object call() throws Exception {
+                // The client needs to be started after the server
+                Thread.sleep(CLIENT_SLEEP);
+                client(TEST_PORT, params);
+                return null;
+            }
+        });
+
+        a.get();
+        b.get();
+    }
 }
--- tests/functional/sessionj/test/functional/valid/NoAliasSocket.sj
+++ tests/functional/sessionj/test/functional/valid/NoAliasSocket.sj
@@ -1,8 +1,51 @@
 package sessionj.test.functional;
 
+import sessionj.runtime.*;
+import sessionj.runtime.net.*;
+import sessionj.runtime.transport.*;
+
+/*
+ * Noalias SJSockets should not be nulled out
+ * after a session operation, like they are
+ * when passed as parameters to a normal method.
+ * (This reproduces issue 4).
+ */
 public class NoAliasSocket extends AbstractValidTest {
 
-    public void run() {
-        assert true;
+    public void client(int port, SJSessionParameters params) throws Exception {
+        final noalias protocol pA { cbegin.!<int>.!<int> }
+
+        final noalias SJService c = SJService.create(pA, "", port);
+        noalias SJSocket s;
+
+        try (s)
+		{
+			s = c.request(params);
+
+			s.send(41);
+		    s.send(42);
+		} finally {
+		}
+    }
+
+    public void server(int port, SJSessionParameters params) throws Exception {
+        final noalias protocol pB { sbegin.?(int).?(int) }
+		final noalias SJServerSocket ss;
+
+		try (ss)
+		{
+            ss = SJServerSocket.create(pB, 1234, params);
+            noalias SJSocket s;
+            try (s) {
+                s = ss.accept();
+
+                int i = s.receiveInt();
+                assert i == 41;
+                i = s.receiveInt();
+                assert i == 42;
+			} finally {
+			}
+		} finally {
+		}
     }
 }
