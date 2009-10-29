4159865d481311fe63c5e224ea79e115d0da2347
omp08 <omp08@pernet.doc.ic.ac.uk>
2009-10-29 20:06:21 +0000

    selector: implemented TCP selector with ahead-of-time select, read and accept in a background thread. still needs changes to SJDefaultSerializer to split up reads ahead of time (broken for now, assumes all values are of the form [count][...bytes...])

--- runtime/src/sessionj/runtime/net/SJSelectorInternal.java
+++ runtime/src/sessionj/runtime/net/SJSelectorInternal.java
@@ -3,5 +3,4 @@ package sessionj.runtime.net;
 public interface SJSelectorInternal extends SJSelectorBase {
     boolean registerAccept(SJServerSocket ss) throws Exception;
     boolean registerInput(SJSocket s) throws Exception;
-    boolean registerOutput(SJSocket s) throws Exception;
 }
