9bc5ef242533fa14a7659c945ff997bfaa1dbad2
Olivier Pernet <o.pernet@gmail.com>
2009-12-02 18:20:10 +0000

    more centralized benchmark scripts

--- benchmark/s100.sh
+++
@@ -1,2 +0,0 @@
-#sessionj -Dsessionj.transports.session=a -Djava.util.logging.config.file=../../logging.properties sessionj/benchmark/SJE/ServerRunner $1 2000 $2 d
-sessionj -Dsessionj.transports.session=a sessionj/benchmark/SJE/ServerRunner $1 2000 $2 d
