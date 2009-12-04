71e759f533ab6a4c0387514c6dcc2dc069aa2598
Olivier Pernet <o.pernet@gmail.com>
2009-12-04 03:13:29 +0000

    fixed script

--- benchmark/build.sh
+++ benchmark/build.sh
@@ -1,9 +1,9 @@
-DIRS="EJava SJE SJthreads TJava"
+DIRS="EJava SJE SJthread TJava"
 for dir in $DIRS; do
     if [ -e $dir/ClientRunner.sj ]; then 
-        sessionjc -d classes $dir/ServerRunner.sj
-        sessionjc -d classes $dir/ClientRunner.sj 
+        sessionjc -sourcepath $dir -d classes $dir/ServerRunner.sj
+        sessionjc -sourcepath $dir -d classes $dir/ClientRunner.sj 
     else
-        javac -d classes $dir/ClientRunner.java $dir/ServerRunner.java
+        javac -sourcepath $dir -d classes $dir/ClientRunner.java $dir/ServerRunner.java
     fi
 done
