de5f46e031da2adf006e4d95bd0d14104ccacd5b
Olivier Pernet <o.pernet@gmail.com>
2009-12-07 00:06:36 +0000

    tidied up logging, speed up when logging disabled

---
+++ runtime/src/sessionj/runtime/transport/AbstractSJTransport.java
@@ -0,0 +1,29 @@
+package sessionj.runtime.transport;
+
+import sessionj.runtime.net.SJSelectorInternal;
+
+public abstract class AbstractSJTransport implements SJTransport {
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof SJTransport)) return false;
+        return getTransportName().equals(((SJTransport) o).getTransportName());
+    }
+
+    /**
+     * Default implementation if async mode is unsupported.
+     * @return null, always
+     */
+    public SJSelectorInternal transportSelector() {
+        return null;
+    }
+
+    public boolean supportsBlocking() {
+        return true;
+    }
+
+}
