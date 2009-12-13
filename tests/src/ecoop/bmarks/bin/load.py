a6b854e56dfd5cb8ea772231398cdb60f976f6ed
dimitris <dimitris@dimitris-laptop>
2009-12-13 17:16:20 +0000

    Final script parameters

--- tests/src/ecoop/bmarks/bin/load.py
+++ tests/src/ecoop/bmarks/bin/load.py
@@ -32,7 +32,7 @@ if debug == 't':
   msgSizes = ['10', '100']
   sessionLengths = ['0', '1', '10']
 else:
-  clients = ['10', '50']
+  clients = ['50', '10']
   msgSizes = ['100', '1000']
   sessionLengths = ['1', '10', '100']
 
