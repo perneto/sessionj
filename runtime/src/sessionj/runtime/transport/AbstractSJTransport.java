47c59c5820c87df792e56111cec17a54589daaee
dimitris <dimitris@dimitris-laptop>
2009-12-14 20:25:23 +0000

    minor

---
+++ runtime/src/sessionj/runtime/transport/AbstractSJTransport.java
@@ -0,0 +1,33 @@
+package sessionj.runtime.transport;
+
+import sessionj.runtime.net.TransportSelector;
+
+public abstract class AbstractSJTransport implements SJTransport {
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof SJTransport)) return false;
+        return getTransportName().equals(((SJTransport) o).getTransportName());
+    }
+
+    /**
+     * Default implementation if async mode is unsupported.
+     * @return null, always
+     */
+    public TransportSelector transportSelector() {
+        return null;
+    }
+
+    public boolean supportsBlocking() {
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName();
+    }
+}
---
+++ tests/src/ecoop/bmarks/smtp/messages/CompileMessagesHack.sj
@@ -0,0 +1,23 @@
+package ecoop.bmarks.smtp.messages;
+
+// Convenience hack to get around a compiler bug for compiling multiple classes in subpackages. 
+public class CompileMessagesHack
+{
+	private SmtpMessage smtpMessage;
+	
+	private DataAck dataAck;
+	private Ehlo ehlo;
+	private EhloAck ehloAck;
+	private EmailAddress emailAddress;
+	private MailAckBody mailAckBody;
+	private MessageBody messageBody;
+	private MessageBodyAck messageBodyAck;
+	private QuitAck quitAck;
+	private RcptAckBody rcptAckBody;
+	private ServerGreeting serverGreeting;
+
+	public CompileMessagesHack()
+	{
+
+	}
+}
