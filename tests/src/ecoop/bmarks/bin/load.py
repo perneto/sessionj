9540adee374b682a02a580842c99d763c7977de5
dimitris <dimitris@dimitris-laptop>
2009-12-14 12:02:13 +0000

    to run ST500

--- tests/src/ecoop/bmarks/bin/load.py
+++ tests/src/ecoop/bmarks/bin/load.py
@@ -23,7 +23,7 @@ msgSizes = []
 sessionLengths = []
 
 if version == 'ALL':
-  versions = ['JT', 'JE', 'ST', 'SE']
+  versions = ['JT', 'JE', 'SE', 'ST']
 else:
   versions = [version]
 
@@ -32,7 +32,7 @@ if debug == 't':
   msgSizes = ['10', '100']
   sessionLengths = ['0', '1', '10']
 else:
-  #clients = ['10', '50']
+#  clients = ['1', '10', '50']
   clients = ['50']
   msgSizes = ['100', '1000']
   sessionLengths = ['1', '10', '100']
--- tests/src/ecoop/bmarks/bin/timer.py
+++ tests/src/ecoop/bmarks/bin/timer.py
@@ -24,7 +24,7 @@ sessionLengths = []
 hostname = socket.gethostname()
 
 if version == 'ALL':
-  versions = ['JT', 'JE', 'ST', 'SE']
+  versions = ['JT', 'JE', 'SE', 'ST']
 else:
   versions = [version]
 
@@ -33,7 +33,7 @@ if debug == 't':
   msgSizes = ['10', '100']
   sessionLengths = ['0', '1', '10']
 else:
-#  clients = ['10', '50']
+  #clients = ['1', '10', '50']
   clients = ['50']
   msgSizes = ['100', '1000']
   sessionLengths = ['1', '10', '100']
