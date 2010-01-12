564b51322e367db2f56d2ee988ad988cddee9deb
Olivier Pernet <o.pernet@gmail.com>
2010-01-12 15:54:03 +0000

    small refactoring

--- runtime/src/sessionj/runtime/transport/AbstractSJConnection.java
+++ runtime/src/sessionj/runtime/transport/AbstractSJConnection.java
@@ -1,20 +1,10 @@
 package sessionj.runtime.transport;
 
-public abstract class AbstractSJConnection implements SJConnection {
-    private final SJTransport transport;
-
+public abstract class AbstractSJConnection extends AbstractWithTransport implements SJConnection {
     protected AbstractSJConnection(SJTransport transport) {
-        this.transport = transport;
+	    super(transport);
     }
-
-    public SJTransport getTransport() {
-        return transport;
-    }
-
-    public String getTransportName() {
-        return transport.getTransportName();
-    }
-
+	
     public boolean supportsBlocking() {
         return true;
     }
